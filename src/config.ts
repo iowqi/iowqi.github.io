d54fca20ae299e2aec1b8b73e2f4669310299da8
iowqi <1485205341@qq.com>
2026-03-25 02:02:59 +0800

    add luminous btn

--- src/config.ts
+++ src/config.ts
@@ -61,8 +61,8 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	name: "iowqi",
+	bio: "An ordinary person, trying to be better everyday. ο(=•ω<=)ρ⌒☆",
 	links: [
 		{
 			name: "Twitter",
